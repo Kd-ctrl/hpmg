7e22ace21d3e23b693fb983c60e2e721e9255010
Kd-ctrl <kaartgikdroidal@gmail.com>
2025-01-13 18:15:12 +0530

    email update

--- hpmg/email_process.py
+++ hpmg/email_process.py
@@ -5,12 +5,13 @@ import json
 @frappe.whitelist()
 def doc_notification(list):
     json_list = json.loads(list)
+    print("==============================", json_list)
     for each_json in json_list:
-        parent_doc_name = each_json.parent_name
-        child_name = each_json.child_name
-        doc_status = each_json.new_status
-        doc_name =  each_json.document_name
-        recepient = each_json.job_applicant,
+        parent_doc_name = each_json["parent_name"]
+        child_name = each_json["child_name"]
+        doc_status = each_json["new_status"]
+        doc_name =  each_json["doc_name"]
+        recepient = each_json["recepient"]
         
         send_mail("Job Offer",parent_doc_name, recepient, f"Your {doc_name} Document has been {doc_status}", f"Reg: Submitted Document {doc_name}")
         
